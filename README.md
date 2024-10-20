893b797dc0ac697d9aa60a98c89032cc4e86b2f0
MrDonBromo <bruno-kevin@hotmail.com>
2024-10-20 12:40:23 -0600

    Streamlit y graficos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TripleTen_Sprint6
 Sprint 6 TripleTen
+Vamos a usar Streamlit para analizar los datos del csv proporcionado de vehiculos y venta de los mismos.
+Se usa pyplot express para graficar y pandas para el dataframe.
---
+++ app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('https://raw.githubusercontent.com/MrDonBromo/TripleTen_Sprint6/refs/heads/main/vehicles_us.csv')
+hist_button = st.button('Crear histograma')
+disp_button = st. button('Crear dispersion')
+
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data,x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+if disp_button:
+    st.write('Creación de un gráfico de dispersión')
+    fig = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig,use_container_width=True)
+
